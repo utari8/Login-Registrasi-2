af9082653bfe00b7325c5c9288ae96df93c7c0b1
F. N. Utari <77846876+utari8@users.noreply.github.com>
2023-06-29 23:54:16 +0700

    menghapus head yang double

--- login.php
+++ login.php
@@ -6,7 +6,6 @@
     <title>Studying Programming</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-light">
